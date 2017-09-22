afdf139c1bb68ddb85458a3f9c45418c8a4038a9
Batuhan Ozkoc <32031460+Batur123@users.noreply.github.com>
2017-09-22 22:05:49 +0300

    Add files via upload

---
+++ Bool Result.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	
+	int a,b;
+	cout<<"Birinci sayi -> "<<endl;
+	cin>>a;
+	cout<<"Ikinci sayi -> "<<endl;
+	cin>>b;
+	
+	bool c= a>10 && b<=100;
+	
+	cout<<"C degeri = "<<c<<endl;
+	
+int x,y;
+	cout<<"Birinci sayi -> "<<endl;
+	cin>>x;
+	cout<<"Ikinci sayi -> "<<endl;
+	cin>>y;
+	
+	bool d= x>10 && y<=100;
+	
+	cout<<"D degeri = "<<d<<endl;
+	
+	
+	
+	
+	
+	return 0;
+}
