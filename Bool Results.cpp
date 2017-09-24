d2679d4872bea2535f8beb4d23e019c0ee666e2e
Batuhan Ozkoc <32031460+Batur123@users.noreply.github.com>
2017-09-24 15:55:35 +0300

    Add files via upload

---
+++ Bool Results.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	
+	bool a=(5<7);
+	bool b=(10==5);
+	bool c=(2!=4);
+	bool d=(0!=0);
+	bool e=(5>2);
+	bool f=(1<2);
+	bool g=(12>=15);
+	
+	cout<<"a degeri: "<< a <<endl;
+	cout<<"b degeri: "<< b <<endl;
+	cout<<"c degeri: "<< c <<endl;
+	cout<<"d degeri: "<< d <<endl;
+	cout<<"e degeri: "<< e <<endl;
+	cout<<"f degeri: "<< f <<endl;
+	cout<<"g degeri: "<< g <<endl;
+	
+	
+	
+	
+	return 0;
+}
