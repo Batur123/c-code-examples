464ca85aff54df8aff27d2bc12111ccd9222b339
Batuhan Ozkoc <32031460+Batur123@users.noreply.github.com>
2017-09-25 22:07:38 +0300

    Add files via upload

---
+++ ExampleFor.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int i;
+	for(i=0;i<=12;i+=3){ //+=3 ifadesi sayýyý 3er 3er arttýrýyor.
+		
+		
+		cout<<i*i*i<<endl; //Küplerini aldýrýyoruz.
+	}
+	
+	
+	
+	return 0;
+}
