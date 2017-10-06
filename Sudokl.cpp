2f625c3bff86fda7a57b3fb029b405bcbd1b3c02
Batuhan Ozkoc <32031460+Batur123@users.noreply.github.com>
2017-10-06 09:45:41 +0300

    Add files via upload

---
+++ Sudokl.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+
+
+	int a[16]={1,2,3,4,3,4,2,1,4,3,1,2,2,1,4,3};
+	
+	cout<<a[0]<<a[1]<<a[2]<<a[3]<<endl;
+	cout<<a[4]<<a[5]<<a[6]<<a[7]<<endl;
+	cout<<a[8]<<a[9]<<a[10]<<a[11]<<endl;
+	cout<<a[12]<<a[13]<<a[14]<<a[15]<<endl;
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
