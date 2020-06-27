385a23d15805c2ba375a510f60660e748f7becda
Batuhan Ozkoc <32031460+Batur123@users.noreply.github.com>
2020-06-27 03:11:09 +0300

    Struct Usage # 1

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string.h>
+#include <locale.h>
+
+
+//using namespace std;
+
+struct Kisi
+{
+	int ID;
+	char ad[30];
+	char soyad[30];
+	char okulno[12];
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	
+	struct Kisi KisiYarat;
+
+	KisiYarat.ID = 1;
+
+	strcpy_s(KisiYarat.ad, "Batuhan");
+	strcpy_s(KisiYarat.soyad, "Özkoç");
+	strcpy_s(KisiYarat.okulno, "16008116032");
+
+	std::cout << "---------- KİŞİ BİLGİLERİ --------------" << std::endl;
+	std::cout << KisiYarat.ad << std::endl << KisiYarat.soyad << std::endl << KisiYarat.okulno << std::endl;
+	std::cout << "---------- KİŞİ BİLGİLERİ --------------";
+	return 0;
+}
+
