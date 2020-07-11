e019e9a15d1295dd63b6040a819b2d0d9904330c
Batuhan Özkoç <32031460+Batur123@users.noreply.github.com>
2020-07-11 03:43:44 +0300

    Update Operator?.cpp

--- Operator?.cpp
+++ Operator?.cpp
@@ -12,7 +12,7 @@ int main()
 	
 	durum = (x%2 == 0) ? "cift" : "tek";
 	
-	x==5 ? cout<<"Sayi 5"<<endl:cout<<"Sayi 5 haric herhangi birsey"<<endl; //Bu neden çalýþtý aklým almýyor.
+	x==5 ? cout<<"Sayi 5"<<endl:cout<<"Sayi 5 haric herhangi birsey"<<endl; //Bu neden calisti aklim almiyor.
 	
 	cout<<durum<<endl;
 	
