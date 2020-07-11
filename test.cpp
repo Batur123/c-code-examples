f82f522bca4d99d6105425145694b72cdb906e22
Batuhan Özkoç <32031460+Batur123@users.noreply.github.com>
2020-07-11 03:42:58 +0300

    ? operator???????

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	string durum;
+	
+	cout<<"Gir?: ";
+	cin>>x;
+	
+	durum = (x%2 == 0) ? "cift" : "tek";
+	
+	x==5 ? cout<<"Sayi 5"<<endl:cout<<"Sayi 5 haric herhangi birsey"<<endl; //Bu neden çalıştı aklım almıyor.
+	
+	cout<<durum<<endl;
+	
+}
